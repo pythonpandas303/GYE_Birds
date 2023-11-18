ea886a3d357e7eae3902961a1ae7c2c6291ddc5c
Tom Teasdale <95242618+pythonpandas303@users.noreply.github.com>
2023-11-18 13:38:04 -0500

    Initial Commit of GYE_Subset.py
    This subsets BBS data to Montana, Idaho, and Wyoming (the states withing the GYE) and merges them with routes.csv, and the species list .csv that is created by SpeciesList_to_CSV (also located in this repository)

---
+++ GYE_Subset.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+Mt = pd.read_csv('Montana.csv')
+Wy = pd.read_csv('Wyoming.csv')
+Id = pd.read_csv('Idaho.csv)
+splist = pd.read_csv('spList.csv')
+
+GreaterYellowstone = pd.concat([Mt, Wy, Id], ignore_index=True, axis=0)
+GreaterYellowstone = pd.merge(GreaterYellowstone, splist, on='AOU', how='left')  
+
+locs = pd.read_csv('routes.csv', encoding='latin-1')
+locs = locs[(locs["StateNum"] == 53) | (locs["StateNum"] == 92) | (locs["StateNum"] == 33)]
+locs = locs.drop('CountryNum', axis=1)
+
+GreaterYellowstone = pd.merge(GreaterYellowstone, locs, on=['StateNum','Route'])
+
+GreaterYellowstone.to_csv('GYE.csv')
